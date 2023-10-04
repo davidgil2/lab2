361066576661006d267c5202e31653eb41aa1cdc
david.gil2 <david.gil2@udea.edu.co>
2023-10-04 18:33:15 -0500

    Primer commit

---
+++ src/test/java/com/udea/lab2/Lab2ApplicationTests.java
@@ -0,0 +1,38 @@
+package com.udea.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class Lab2ApplicationTests {
+
+	@Autowired
+	DataController dataController;
+
+	@Test
+	void health() {
+		assertEquals("HEALTH CHECK OK", dataController.healthCheck());
+	}
+
+	@Test
+	void version() {
+		assertEquals("THE ACTUAL VERSION IS 1.0.0", dataController.version());
+	}
+
+	@Test
+	void nationLength() {
+		Integer nationsLength = dataController.getRandomNations().size();
+		assertEquals(10, nationsLength);
+	}
+
+	@Test
+	void currenciesLength() {
+		Integer currenciesLength = dataController.getRandomnCurrencies().size();
+		assertEquals(20, currenciesLength);
+	}
+
+
+}
